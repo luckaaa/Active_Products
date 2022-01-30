d54c7921a14aa3550222d62a0acb727ebf686a86
luka <98187912+luckaaa@users.noreply.github.com>
2022-01-30 13:03:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import csv
 import pandas as pd
 
 with open("active_products.csv") as file:
-    reader = csv.reader(file)
+    active_products = csv.reader(file)
 
     list_of_active_pr = []
 
-    for row in reader:
+    for row in active_products:
         list_of_active_pr.append(row[4])
 
 stock_list = []
 
-stock = pd.read_csv("stock.csv", encoding="mac_roman", delimiter=";") #if using Windows, use encoding="cp1252"
+stock = pd.read_csv("stock.csv", encoding="mac_roman", delimiter=";")
 for row in stock["Title"]:
     stock_list.append(row)
 
@@ -23,9 +23,24 @@ for bottle in stock_list:
     else:
         continue
 
+tasting_samples = [s for s in inactive_bottles_list if "(Tasting sample)" in s]
+tasting_Samples = [s for s in inactive_bottles_list if "(Tasting Sample)" in s]
+
+DBM = []
+for dbm in inactive_bottles_list:
+    if dbm.find("Deer, Bear & Moose") != -1:
+        DBM.append(dbm)
+for dbm in DBM:
+    inactive_bottles_list.remove(dbm)
+
+for samples in tasting_samples:
+   inactive_bottles_list.remove(samples)
+
+for Samples in tasting_Samples:
+    inactive_bottles_list.remove(Samples)
 
 for inactive_bottle in inactive_bottles_list:
     print(inactive_bottle)
 print(f"Number of inactive bottles: {len(inactive_bottles_list)}\n")
 
-print("Please note that number might be higher because of included:\ntasting samples, Vault bottles, weird characters and metabase discrepancies (some bottles are actually active)")
+print("Please note that number might be higher because of included:\Vault bottles, weird characters and metabase discrepancies (some bottles are actually active).")
